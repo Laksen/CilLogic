16dfa08d5ace7a4eae3e2b2e05813b1a19fb6f30
Jeppe Johansen <jeppe@j-software.dk>
2018-05-10 11:19:38 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CilLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
